6958cbe85878668337f3cab1ac08b23268553279
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2020-08-17 21:14:31 +0100

    dropdown added

--- assets/js/campaign-dashboard.js
+++ assets/js/campaign-dashboard.js
@@ -6,3 +6,18 @@ togglers.forEach((toggler) => {
       .parentElement.classList.toggle("collapsed-sidebar");
   });
 });
+// Dropdown
+const dropdownBtn = document.querySelector("#dropdownBtn");
+const dropdown = document.querySelector(".dropdown-holder");
+dropdownBtn.addEventListener("click", (e) => {
+  dropdown.classList.toggle("closed");
+});
+// Click Ouside
+var specifiedElement = document.querySelector(".avatar-section");
+
+//I'm using "click" but it works with any event
+document.addEventListener("click", function (event) {
+  var isClickInside = specifiedElement.contains(event.target);
+
+  !isClickInside && dropdown.classList.add("closed");
+});
